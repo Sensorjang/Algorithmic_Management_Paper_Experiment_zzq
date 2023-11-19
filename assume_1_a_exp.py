30c472112663051c643540efb93997bb6864bb2e
Sensorjang <515310624@qq.com>
2023-11-19 16:24:36 +0800

    Update Drawing

--- assume_1_a_exp.py
+++ assume_1_a_exp.py
@@ -8,19 +8,10 @@ num_routines = 10
 num_variants = 10
 num_stages = 10
 
-# 初始化矩阵Z
-Z = np.random.randint(1, 7, size=(num_workers, num_routines * num_variants))
-indices = np.random.choice(num_workers * num_routines * num_variants, int(num_workers * num_routines * num_variants * 0.1), replace=False)
-Z.ravel()[indices] = np.random.randint(6, 11, size=int(num_workers * num_routines * num_variants * 0.1))
-
-# 初始化变化函数参数
-change_prob = [0.7, 0.3]  # 大于5的值替换的新值的概率分布
-change_range = [5, 10]  # 大于5的值替换的新值的范围
-
 
 # 选择数值函数
 stage_mem = np.full((num_stages, num_workers), -1, dtype=int)
-def select_value(worker_index, stage, is_transparent):
+def select_value(worker_index, stage, is_transparent, Z):
     if is_transparent:
         top_10_percent = int(num_routines * num_variants * 0.1)
         sorted_indices = np.argsort(Z[:, worker_index])
@@ -57,37 +48,57 @@ def refresh_Z(Z):
     Z[indices_less_than_5] = np.random.randint(1, 11, size=len(indices_less_than_5[0]))
 
 
-# 初始化存储变量
-worker_scores = np.zeros((num_workers, num_stages))
-platform_scores = np.zeros(num_stages)
+def run():
+    # 初始化存储变量
+    worker_scores = np.zeros((num_workers, num_stages))
+    platform_scores = np.zeros(num_stages)
 
-# 进行实验
-for stage in range(num_stages):
-    # 生成随机的工作人员执行顺序
-    worker_order = list(range(num_workers))
-    random.shuffle(worker_order)
-    # 按随机顺序执行select_value函数
-    for worker_index in worker_order:
-        is_transparent = np.random.rand() < 0.8  # 80%的概率为透明，20%的概率为非透明
-        worker_scores[worker_index][stage] = select_value(worker_index, stage, is_transparent)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(8, 8))
 
-    if stage < num_stages - 1:
-        refresh_Z(Z)
-    # 计算平台得分
-    platform_scores[stage] = np.sum(worker_scores[:, stage])
+    # 初始化矩阵Z
+    Z = np.random.randint(1, 7, size=(num_workers, num_routines * num_variants))
+    indices = np.random.choice(num_workers * num_routines * num_variants,
+                               int(num_workers * num_routines * num_variants * 0.1), replace=False)
+    Z.ravel()[indices] = np.random.randint(6, 11, size=int(num_workers * num_routines * num_variants * 0.1))
 
-# 绘制每个劳动者得分变化图
-for worker_index in range(num_workers):
-    plt.plot(range(num_stages), worker_scores[worker_index, :], label=f"Worker {worker_index}")
-plt.xlabel("Stage")
-plt.ylabel("Score")
-plt.title("Individual Worker Scores")
-plt.legend()
-plt.show()
+    # 重置 stage_mem 矩阵
+    stage_mem = np.full((num_stages, num_workers), -1, dtype=int)
 
-# 绘制平台得分变化图
-plt.plot(range(num_stages), platform_scores)
-plt.xlabel("Stage")
-plt.ylabel("Score")
-plt.title("Platform Scores")
-plt.show()
+    # 进行实验
+    for stage in range(num_stages):
+        # 生成随机的工作人员执行顺序
+        worker_order = list(range(num_workers))
+        random.shuffle(worker_order)
+        # 按随机顺序执行select_value函数
+        for worker_index in worker_order:
+            is_transparent = np.random.rand() < 0.8  # 80%的概率为透明，20%的概率为非透明
+            worker_scores[worker_index][stage] = select_value(worker_index, stage, is_transparent, Z)
+
+        if stage < num_stages - 1:
+            refresh_Z(Z)
+        # 计算平台得分
+        platform_scores[stage] = np.sum(worker_scores[:, stage])
+
+    # 绘制每个劳动者得分变化图
+    for worker_index in range(num_workers):
+        ax1.plot(range(num_stages), worker_scores[worker_index, :], label=f"Worker {worker_index}")
+
+    ax1.set_xlabel("Stage")
+    ax1.set_ylabel("Score")
+    ax1.set_title("Individual Worker Scores")
+    ax1.legend()
+
+    # 绘制平台得分变化图
+    ax2.plot(range(num_stages), platform_scores)
+    ax2.set_xlabel("Stage")
+    ax2.set_ylabel("Score")
+    ax2.set_title("Platform Scores")
+
+    # 调整子图之间的间距
+    fig.tight_layout()
+    # 显示图形
+    plt.show()
+    return
+
+if __name__ == '__main__':
+    run()
